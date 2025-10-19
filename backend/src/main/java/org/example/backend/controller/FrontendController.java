f8efdcd633932f30328125126f5357e0954cc90b
youmnaalkadi1997 <youmna.alkadi1997@gmail.com>
2025-10-19 21:56:03 +0200

    add some changes

--- backend/src/main/java/org/example/backend/controller/FrontendController.java
+++ backend/src/main/java/org/example/backend/controller/FrontendController.java
@@ -2,10 +2,11 @@ package org.example.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FrontendController {
-    @RequestMapping(value = { "/", "/search" })
+    @RequestMapping(value = { "/", "/search" }, method = RequestMethod.GET)
     public String index() {
         return "forward:/index.html";
     }
